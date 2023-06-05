0e1b3e99a6e68f3e581b0b177969e238fdc11869
Joydip <joydip.mondal@gmail.com>
2023-06-05 11:13:44 +0530

    simple js code

---
+++ codes/day-1/web-app/index.js
@@ -0,0 +1,11 @@
+//function main(){
+function changeContent() {
+    var paraObj = document.getElementById("mainPara")
+    if (paraObj)
+        paraObj.innerText = "changed content"
+}
+//}()
+
+// const fsModule = require('fs')
+// fsModule.readFile
+console.log('welcome to JS')
