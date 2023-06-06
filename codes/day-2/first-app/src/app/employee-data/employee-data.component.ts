a46606bc267adedee9c806d32b9cddddd99f3f4a
Joydip <joydip.mondal@gmail.com>
2023-06-06 14:46:34 +0530

    assignment uploaded with sample code

--- codes/day-2/first-app/src/app/employee-data/employee-data.component.ts
+++ codes/day-2/first-app/src/app/employee-data/employee-data.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { Employee } from "../models/employee";
 
 @Component({
     selector: 'app-employee-data',
     templateUrl: './employee-data.component.html'
 })
 export class EmployeeDataComponent {
-    employeeObj = { id: 1, salary: 2000, name: 'anil' }
+    employeeObj?: Employee[] = [
+        { id: 1, name: 'anil', salary: 1000 },
+        { id: 2, salary: 2000, name: 'sunil' }
+    ]
+
+    // iterate() {
+    //     for (let obj of this.employeeObj) {
+    //         console.log(obj.)
+    //     }
+    // }
 }
---
+++ codes/day-2/first-app/src/app/models/employee.ts
@@ -0,0 +1,11 @@
+// export class Employee {
+//     constructor(public id: number, public name: string, public salary: number) {
+
+//     }
+// }
+
+export interface Employee {
+    id: number;
+    name: string;
+    salary: number;
+}
