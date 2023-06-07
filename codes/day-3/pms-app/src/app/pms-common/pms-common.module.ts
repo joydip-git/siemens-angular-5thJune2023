178b577fffa1280e02eb98c376d243662edcb8bf
Joydip <joydip.mondal@gmail.com>
2023-06-07 11:51:37 +0530

    pms-app update-3

--- codes/day-3/pms-app/src/app/pms-common/pms-common.module.ts
+++ codes/day-3/pms-app/src/app/pms-common/pms-common.module.ts
@@ -4,6 +4,17 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+import { Routes } from "@angular/router";
+
+const commonRoutes: Routes = [
+  {
+    path: '', component:
+  },
+  {},
+  {}
+]
+
+
 @NgModule({
   declarations: [
     HomeComponent,
